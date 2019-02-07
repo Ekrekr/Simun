21f3a94fd0b7eaae5a78b607b34a79c9904fd556
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-02-07 10:06:33 +0000

    Added basic structure as suggested by Ian

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Peter
+# Simon
 A website for experimenting with agent sharing. Client and server side built on Node JS.
---
+++ server.js
@@ -0,0 +1,69 @@
+// Sample express web server.  Supports the same features as the provided server,
+// and demonstrates a big potential security loophole in express.
+
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var banned = [];
+banUpperCase("./public/", "");
+
+// Define the sequence of functions to be called for each request.  Make URLs
+// lower case, ban upper case filenames, require authorisation for admin.html,
+// and deliver static files from ./public.
+app.use(lower);
+app.use(ban)
+app.use("/admin.html", auth);
+var options = { setHeaders: deliverXHTML };
+app.use(express.static("public", options));
+app.listen(8080, "localhost");
+console.log("Visit http://localhost:8080/");
+
+// Make the URL lower case.
+function lower(req, res, next) {
+    req.url = req.url.toLowerCase();
+    next();
+}
+
+// Forbid access to the URLs in the banned list.
+function ban(req, res, next) {
+    for (var i=0; i<banned.length; i++) {
+        var b = banned[i];
+        if (req.url.startsWith(b)) {
+            res.status(404).send("Filename not lower case");
+            return;
+        }
+    }
+    next();
+}
+
+// Redirect the browser to the login page.
+function auth(req, res, next) {
+    res.redirect("/login.html");
+}
+
+// Called by express.static.  Deliver response as XHTML.
+function deliverXHTML(res, path, stat) {
+    if (path.endsWith(".html")) {
+        res.header("Content-Type", "application/xhtml+xml");
+    }
+}
+
+// Check a folder for files/subfolders with non-lowercase names.  Add them to
+// the banned list so they don't get delivered, making the site case sensitive,
+// so that it can be moved from Windows to Linux, for example. Synchronous I/O
+// is used because this function is only called during startup.  This avoids
+// expensive file system operations during normal execution.  A file with a
+// non-lowercase name added while the server is running will get delivered, but
+// it will be detected and banned when the server is next restarted.
+function banUpperCase(root, folder) {
+    var folderBit = 1 << 14;
+    var names = fs.readdirSync(root + folder);
+    for (var i=0; i<names.length; i++) {
+        var name = names[i];
+        var file = folder + "/" + name;
+        if (name != name.toLowerCase()) banned.push(file.toLowerCase());
+        var mode = fs.statSync(root + file).mode;
+        if ((mode & folderBit) == 0) continue;
+        banUpperCase(root, file);
+    }
+}
