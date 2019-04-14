50a0d7ab49df4141f006757ad0e50085483725d9
Unknown <ja16426@my.bristol.ac.uk>
2019-04-14 11:07:29 +0100

    Updated using comments of PR
    Have made changes to the database call structure, npm run dev now properly displays the image, made changes to README file to describe how to view and schema

--- site/server/server.js
+++ site/server/server.js
@@ -1,161 +1,163 @@
-var http = require("http");
+var http = require('http')
 
 // Called every time a request is received by the server. Receives and emits
 // actions as a consequence. Event interfaces are:
 // 'connect', 'connection', 'request', and 'upgrade'.
-var server = http.createServer();
+var server = http.createServer()
 
 // Placeholder until real tests are needed. This is how to export functions for
 // testing.
 module.exports = {
   exampleFunc: function exampleFunc(input) {
-    return !input;
+    return !input
+  },
+  putData: function putData(input) {
+    return input
+  },
+  getData: function getData(input) {
+    return input
+  },
+  updateData: function updateData(input) {
+    return input
+  },
+  deleteData: function deleteData(input) {
+    return input
   }
-};
+}
 
 // request - Emitted for Each request from the client (We would listen here).
-server.on("request", (request, response) => {
-  var body = [];
+server.on('request', (request, response) => {
+  var body = []
   request
-    .on("data", chunk => {
-      body.push(chunk);
+    .on('data', chunk => {
+      body.push(chunk)
     })
-    .on("end", () => {
-      body = body.concat.toString();
-      console.log("Server received ", body.toString());
+    .on('end', () => {
+      body = body.concat.toString()
+      console.log('Server received ', body.toString())
     })
-    .on("error", err => {
-      console.error(err);
-      response.statusCode = 400;
-      response.end();
-    });
+    .on('error', err => {
+      console.error(err)
+      response.statusCode = 400
+      response.end()
+    })
 
-  response.on("error", err => {
-    console.error(err);
-  });
+  response.on('error', err => {
+    console.error(err)
+  })
 
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "application/json");
-  response.write("Hello World!");
-  response.end();
-});
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'application/json')
+  response.write('Hello World!')
+  response.end()
+})
 
-server.on("error", console.error);
+server.on('error', console.error)
 
 // connect - Raised for all the ‘connect’ request by the HTTP client.
-server.on("connect", (request, response) => {});
+server.on('connect', (request, response) => {})
 
 // connection - Emitted when a new TCP stream is established. Provide access to
 // the socket established.
-server.on("connection", (request, response) => {});
+server.on('connection', (request, response) => {})
 
 // upgrade -  each time a client requests an upgrade of the protocol (can be
 // HTTP version).
-server.on("upgrade", (request, response) => {});
+server.on('upgrade', (request, response) => {})
 
 /*****************************************/
 /*               SERVER                  */
 /*****************************************/
 
-var path = require("path");
+var path = require('path')
 
-const express = require('express');
+const express = require('express')
 // const people = require('./people.json');
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
-// app.set("views", path.join(__dirname, "views"));
-
-// app.use(express.static(__dirname + '/public'));
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '../models/public/views'))
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Homepage',
+    title: 'Homepage'
     // user: 'something'
-  });
-});
+  })
+})
 
 const servers = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address()}`);
-});
+  console.log(`Express running → PORT ${server.address()}`)
+})
 
 /*****************************************/
 /*              Database                */
 /*****************************************/
 
-const sqlite3 = require("sqlite3").verbose();
-const dbPath = path.resolve(__dirname, "../database/database.db");
+const sqlite3 = require('sqlite3').verbose()
+const dbPath = path.resolve(__dirname, '../database/database.db')
 let db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
-  console.log('Connected to the Login database.');
-});
-//GET
-get_data('Jadams');
+  console.log('Connected to the Login database.')
+})
+// GET
+getData('Login', '1')
 
-function get_data(forename) {
-
-  // db.serialize((id) => {
+function getData(Table, lookup) {
   db.each(`SELECT *
-                  FROM Login
-                  WHERE forename = ?`, forename, (err, row) => {
+                  FROM ` + Table + `
+                  WHERE id = ?`, lookup, (err, row) => {
     if (err) {
-      console.error(err.message);
+      console.error(err.message)
     }
-    console.log(row.id + "\t" + row.forename);
-  });
+    console.log(row.id + '\t' + row.forename + '\t' +
+      row.surname + '\t' + row.username + '\t' +
+      row.password)
+  })
   // });
 }
-//PUT
-//put_data('mr', 'jones', 'bla', 'hi');
+// PUT
+function putData(Table, forname, surname, username, password) {
+  data = [forname, surname, username, password]
+  var sqlPut = `INSERT INTO ` + Table + ` (forename, surname, username, password) VALUES (?,?,?,?)`
+  console.log(sql)
 
-function put_data(forname, surname, username, password) {
-  data = [forname, surname, username, password];
-  var sql = 'INSERT INTO Login (forename, surname, username, password) VALUES (?,?,?,?)';
-
-  // output the INSERT statement
-  console.log(sql);
-
-  db.run(sql, data, function (err) {
+  db.run(sqlPut, data, function (err) {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    console.log(`Rows inserted ${this.changes}`);
-  });
+    console.log(`Rows inserted ${this.changes}`)
+  })
 }
-//UPDATE
-//update_data('James', 'Jadams') - Example
-function update_data(original, change) {
-  data = [change, original]
-  let sql_update = `UPDATE Login
-            SET forename = ?
-            WHERE forename = ?`;
+// UPDATE
+function updateData(Table, lookup, change) {
+  data = [change, lookup]
+  let sqlUpdate = `UPDATE ` + Table +
+    `SET forename = ?
+            WHERE id = ?`
 
-  db.run(sql_update, data, function (err) {
+  db.run(sqlUpdate, data, function (err) {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    console.log(`Row(s) updated: ${this.changes}`);
-
-  });
+    console.log(`Row(s) updated: ${this.changes}`)
+  })
 }
-//DELETE
-function delete_row(id) {
-  //id = 100
-  db.run(`DELETE FROM Login WHERE id=?`, id, function (err) {
+// DELETE
+function deleteRow(Table, lookup) {
+  db.run(`DELETE FROM ` + Table + ` WHERE id=?`, lookup, function (err) {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    console.log(`Row(s) deleted ${this.changes}`);
-  });
+    console.log(`Row(s) deleted ${this.changes}`)
+  })
 }
 
 // close the database connection
 db.close((err) => {
   if (err) {
-    return console.error(err.message);
+    return console.error(err.message)
   }
-});
+})
--- site/tests/server_tests.js
+++ site/tests/server_tests.js
@@ -8,18 +8,36 @@
 var expect = require('chai').expect
 var server = require('../server/server')
 
-describe('server.exampleFunc()', function () {                       // eslint-disable-line
+describe('server.exampleFunc()', function () { // eslint-disable-line
   it('should return the opposite belief of the input', function () { // eslint-disable-line
     // 1. ARRANGE
     var expectA = true
     var expectB = false
 
-    // 2. ACT
-    var resultA = server.exampleFunc(false)
-    var resultB = server.exampleFunc(true)
 
     // 3. ASSERT
     expect(resultA).to.be.equal(expectA)
     expect(resultB).to.be.equal(expectB)
+  })
+})
+
+describe('server.DatabaseFunctions()', function () {
+  it('should return the output described', function () {
+    var expectC = `James, Adams, Jadams, Maybe`
+    var expectD = `Row(s) inserted: 1`
+    var expectE = `Row(s) updated: 1`
+    var expectF = `Row(s) deleted: 1`
+    // 2. ACT
+    var resultA = server.exampleFunc(false)
+    var resultB = server.exampleFunc(true)
+    var resultC = server.getData('Login', 1)
+    var resultD = server.putData('Login', 'Test1', 'Test2', 'Test3', 'Test4')
+    var resultE = server.updateData('Login', 'James', '1')
+    var resultF = server.deleteData('Login', '2')
+
+    expect(resultC).to.be.equal(expectC)
+    expect(resultD).to.be.equal(expectD)
+    expect(resultE).to.be.equal(expectE)
+    expect(resultF).to.be.equal(expectF)
   })
 })
