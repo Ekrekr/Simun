d289348424968dda541096592a9f28829b4f2ccb
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-04-20 23:19:44 +0100

    Fixed bug with selected content loading too late

--- site/public/scripts/receive.js
+++ site/public/scripts/receive.js
@@ -8,39 +8,37 @@ var $ = function (id) { return document.getElementById(id) }
 
 var userID = 0
 
-// Retrieves data by asking the server for it.
-function retrieveData (table, id, _callback) {
-  request('http://localhost:7000/data/' + table + "/" + id, { json: true }, (err, res, body) => {
-    if (err) { return _callback(err) }
-    return _callback(null, JSON.parse(JSON.stringify(body)));
-  })
-}
+// // Retrieves data by asking the server for it.
+// function retrieveData (table, id, _callback) {
+//   request('http://localhost:7000/data/' + table + "/" + id, { json: true }, (err, res, body) => {
+//     if (err) { return _callback(err) }
+//     return _callback(null, JSON.parse(JSON.stringify(body)));
+//   })
+// }
 
-usersnippets = []
+// // First retrieve the redirect on which snippets the user currently has.
+// retrieveData('redirect', 0, function(err, redirect) {
+//   var snippets = JSON.parse(redirect.snippetids)
 
-// First retrieve the redirect on which snippets the user currently has.
-retrieveData('redirect', 0, function(err, redirect) {
-  var snippets = JSON.parse(redirect.snippetids)
+//   // For each snippet, retrieve the snippet content ID.
+//   snippets.forEach(function(entry, index) {
+//     retrieveData('snippets', entry, function(err, snippet) {
 
-  // For each snippet, retrieve the snippet content ID.
-  snippets.forEach(function(entry, index) {
-    retrieveData('snippets', entry, function(err, snippet) {
+//       // Retrieve the snippet content
+//       retrieveData('snippetcontent', snippet.contentid, function(err, snippetcontent) {
 
-      // Retrieve the snippet content
-      retrieveData('snippetcontent', snippet.contentid, function(err, snippetcontent) {
+//         // Want to populate the main content with the first snippet by default.
+//         if (index == 0) {
+//           $('selected-description').innerHTML = snippetcontent.description
+//           $('selected-content').src = snippetcontent.content
+//         }
 
-        // Want to populate the main content with the first snippet by default.
-        if (index == 0) {
-          $('selected-description').innerHTML = snippetcontent.description
-          $('selected-content').src = snippetcontent.content
-        }
-
-        // Push all snippets to an array 
-        usersnippets.push({})
-      })
-    })
-  })
-})
+//         // Push all snippets to an array 
+//         usersnippets.push({})
+//       })
+//     })
+//   })
+// })
 
 // Assign snippet data.
 
--- site/server/server.js
+++ site/server/server.js
@@ -1,13 +1,30 @@
 var path = require('path')
 const express = require('express')
 var database = require('./database.js')
-const fetch = require('node-fetch')
+const request = require('request')
 
 const app = express()
 
 app.set('views', path.join(__dirname, '../models/public'))
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, '../public')))
+
+// Retrieve data from the database
+app.get('/data/:table/:id', (req, res) => {
+  console.log('Retrieving data from table "' + req.params.table + '" and id', req.params.id)
+
+  database.getData(req.params.table, req.params.id).then( response => {
+    res.send(JSON.stringify(response[0]))
+  });
+})
+
+// Retrieves data by asking the server for it.
+function retrieveData (table, id, _callback) {
+  request('http://localhost:7000/data/' + table + "/" + id, { json: true }, (err, res, body) => {
+    if (err) { return _callback(err) }
+    return _callback(null, JSON.parse(JSON.stringify(body)));
+  })
+}
 
 // Page retrieval
 app.get('/', (req, res) => {
@@ -19,11 +36,40 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/receive', (req, res) => {
-  res.render('receive', {
-    snippets: 
-    [{'description': 'example 1', 'content': 'assets/images/snippet-example-1.png'},
-    {'description': 'example 2', 'content': 'assets/images/snippet-example-2.png'}]
-  });
+  // File to pass to pug to tell it what value to give variables.
+  variables = {}
+  variables.selected = {}
+  variables.snippets = []
+
+  // Need to load snippet data from the database to display on the page.
+  retrieveData('redirect', 0, function(err, redirect) {
+    var snippets = JSON.parse(redirect.snippetids)
+
+    // For each snippet, retrieve the snippet content ID.
+    snippets.forEach(function(entry, index) {
+      retrieveData('snippets', entry, function(err, snippet) {
+
+        // Retrieve the snippet content.
+        retrieveData('snippetcontent', snippet.contentid, function(err, snippetcontent) {
+          variables.snippets.push({'description': snippetcontent.description, 
+          'content': snippetcontent.content})
+
+          // Want to populate the main content with the first snippet by default.
+          if (index == 0) {
+            variables.selected.description = snippetcontent.description
+            variables.selected.content = snippetcontent.content
+          }
+
+          // Only return final source if final iteration.
+          if (index == snippets.length - 1) {
+            // Send the created page back to user after loading all the variables,
+            // with a slight delay to prevent further problems.
+            setTimeout(function() { res.render('receive', variables) }, 100);
+          }
+        })
+      })
+    })
+  })
 })
 
 app.get('/send', (req, res) => {
@@ -32,15 +78,6 @@ app.get('/send', (req, res) => {
 
 app.get('/stats', (req, res) => {
   res.render('stats.html')
-})
-
-// Snippet handling
-app.get('/data/:table/:id', (req, res) => {
-  console.log('Retrieving data from table "' + req.params.table + '" and id', req.params.id)
-
-  database.getData(req.params.table, req.params.id).then( response => {
-    res.send(JSON.stringify(response[0]))
-  });
 })
 
 // Start the server.
