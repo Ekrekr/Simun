68754a7cb4794b7f1a54f21889707b30e4d7031c
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-04-28 17:03:13 +0100

    Added some descriptions of snippet logic calls, updated database to force not null and unique in required areas

--- site/server/snippet-logic.js
+++ site/server/snippet-logic.js
@@ -8,21 +8,41 @@ module.exports = {
 
 // Forwards a snippet on to two separate users.
 async function forwardSnippet (snippetid, username) {
-  var snippet = await database.getData('snippet', snippetid)
-
   // Select two redirect entries at random.
   // Create two new snippets with the same content id, same first owner, the username of the current user, and an increased forward count.
-  // Append the new snippet IDs 
+  // Append the new snippet IDs to the redirects list of owned snippets.
+  // Delete the original snippet.
+  // Force update the accounts attached to the redirect.
+
+  var snippet = await database.getData('snippet', snippetid)
+
+  var redirectid1 = 0;
 
   // Need to create two new snippets.
-  await database.putSnippetData(snippet.contentid, snippet.firstowner, username, snippet.forwardcount + 1).then( async (res) => {
+  await database.putSnippetData(snippet.contentid, redirectid1, snippet.firstowner, username, snippet.forwardcount + 1).then( async (res) => {
     console.log("put snippet insert: ", res)
 
     // Once made, assign to a random user.
 
   })
 
-  
-
   return true
+}
+
+async function deleteSnippet (snippetid, accountid) {
+  // Retrieve the redirect entry associated with the account ID.
+  // Remove the entry for the snippet id from the redirect list of snipped IDs.
+  // Remove the snippet entry.
+  // Optional: If it's the last snippet then delete the content. This would probably be better done with a garbage collector.
+}
+
+async function sendSnippet (snippetInfo, usernamealias) {
+  // Select a redirect entry at random, retrieving the redirect ID.
+  // Create a new snippet content of snippetInfo, storing the snippet content ID.
+  // Create a new snippet with this snippet content id, the redirect ID, same first owner, the username of the current user, and an increased forward count.
+}
+
+async function createSnippetContent (content) {
+  // Create new snippet content with content.
+  // Return the ID of this new snippet content.
 }
