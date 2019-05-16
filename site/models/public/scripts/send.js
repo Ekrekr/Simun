96d8c82205f4b48c0e76e36bebcd8425c776511f
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-05-16 16:05:35 +0100

    Fixed standard syntax issues

--- site/models/public/scripts/send.js
+++ site/models/public/scripts/send.js
@@ -1,7 +1,3 @@
-var tools = require('./tools.js')
-
-var currentlyActive = 0
-
 // Shorthand for getting elements by ID.
 var $ = function (id) { return document.getElementById(id) }
 
@@ -14,27 +10,23 @@ fileField.addEventListener('change', (e) => {
   var selectedFile = fileField.files[0]
 
   // Once the file has loaded, read it.
-  var reader = new FileReader();
-  reader.readAsDataURL(selectedFile);
+  var reader = new window.FileReader()
+  reader.readAsDataURL(selectedFile)
   reader.onload = (e) => {
-    fileBackground.style.backgroundImage = 'url(' + e.target.result + ')';
+    fileBackground.style.backgroundImage = 'url(' + e.target.result + ')'
 
     // Retreive the image dimensions then scale the upload box background from there.
-    var img = new Image;
-    img.src = reader.result;
+    var img = new Image()
+    img.src = reader.result
     img.onload = () => {
-      console.log("The width of the image is " + img.width + "px.");
       // If the image is wider than it is tall, then to maximise the area occupied set the width
-      // to the width of the divider and the height to the maxium height while keeping the 
+      // to the width of the divider and the height to the maxium height while keeping the
       // correct aspect ratio. Do the opposite if the image is taller than it is wide.
       if (img.width >= (img.height * 2.0)) {
-        console.log("The image is wider than it is tall")
-        fileBackground.style.height = (img.height / img.width * 400) + "px"
+        fileBackground.style.height = (img.height / img.width * 400) + 'px'
         fileBackground.style.width = 400
       } else {
-        console.log("The image is taller than it is wide")
-        console.log("setting height to " + img.width / img.height * fileBackground.style.height)
-        fileBackground.style.width = (img.width / img.height * 200) + "px"
+        fileBackground.style.width = (img.width / img.height * 200) + 'px'
         fileBackground.style.height = 200
       }
     }
