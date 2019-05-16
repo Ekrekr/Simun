16b5d8b4b31483d3bf8f15dd837442bdd200da43
Unknown <ja16426@my.bristol.ac.uk>
2019-05-16 18:43:12 +0100

    Serverside cookies implemented -Testing
    Can now be reviewed for server side cookies, testing still needs to be implemented

--- site/server/server.js
+++ site/server/server.js
@@ -13,9 +13,9 @@ var fs = require('fs')
 var https = require('https')
 // var certificate = fs.readFileSync('../client-key.pem').toString();
 module.exports = {
-    connectToServer: connectToServer,
-    generateJWT: generateJWT,
-    verifyUserViaAlias: verifyUserViaAlias
+  connectToServer: connectToServer,
+  generateJWT: generateJWT,
+  verifyUserViaAlias: verifyUserViaAlias
 }
 connectToServer()
 
@@ -25,7 +25,7 @@ connectToServer()
 
 // Enables REST communication with server.
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }))
 app.set('views', path.join(__dirname, '../models/public'))
 app.set('view engine', 'pug')
@@ -34,18 +34,11 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/', router)
 // Used to check current user against the cookie token
-router.use(function (req, res, next) {
-    // console.log("Gets Here123")
-    // var tokens = req.body.token || req.query.token || req.headers['x-access-token']
-    // console.log("Tokens:")
-    // console.log(tokens)
-    next()
-})
 
 async function connectToServer() {
-    app.listen(7000, 'localhost', () => {
-        console.log('server: Express running → localhost:7000')
-    })
+  app.listen(7000, 'localhost', () => {
+    console.log('server: Express running → localhost:7000')
+  })
 }
 
 /// ///////////////////////////////////////////////
@@ -53,24 +46,24 @@ async function connectToServer() {
 /// ///////////////////////////////////////////////
 
 router.get('/snippetcontent/:id', (req, res) => {
-    console.log('server: Retrieving snippet content with id:', req.params.id)
-    database.getSnippetContent(req.params.id).then(response => {
-        res.send(JSON.stringify(response[0]))
-    })
+  console.log('server: Retrieving snippet content with id:', req.params.id)
+  database.getSnippetContent(req.params.id).then(response => {
+    res.send(JSON.stringify(response[0]))
+  })
 })
 
 router.post('/forward-snippet/', (req, res) => {
-    console.log('server: Forwarding snippet id:', req.body.snippetid)
-    database.forwardSnippet(req.body.snippetid).then(res => {
-        return res
-    })
+  console.log('server: Forwarding snippet id:', req.body.snippetid)
+  database.forwardSnippet(req.body.snippetid).then(res => {
+    return res
+  })
 })
 
 router.post('/create-snippet/', (req, res) => {
-    console.log('server: Creating snippet with content:', req.body.content, 'description:', req.body.description, 'redirectid:', req.body.redirectid)
-    database.createSnippet(req.body.content, req.body.description, req.body.redirectid).then(res => {
-        return res
-    })
+  console.log('server: Creating snippet with content:', req.body.content, 'description:', req.body.description, 'redirectid:', req.body.redirectid)
+  database.createSnippet(req.body.content, req.body.description, req.body.redirectid).then(res => {
+    return res
+  })
 })
 
 /// ///////////////////////////////////////////////
@@ -80,146 +73,140 @@ router.post('/create-snippet/', (req, res) => {
 // The next 2 requests include the authentication
 // Of the current user
 router.get('/', (req, res) => {
-    try {
-        var token = new Cookies(req, res).get('currentUser')
-        let decoded = jwt.verify(token, config.secret)
-        verifyUserViaAlias(res, res, decoded.data)
-    } catch (e) {
-        res.render('login')
-    }
+  try {
+    var token = new Cookies(req, res).get('currentUser')
+    let decoded = jwt.verify(token, config.secret)
+    verifyUserViaAlias(res, res, decoded.data)
+  } catch (e) {
+    res.render('login')
+  }
 })
 
 router.get('/login', (req, res) => {
-    try {
-        var token = new Cookies(req, res).get('currentUser')
-        console.log(res)
-        let decoded = jwt.verify(token, config.secret)
-        verifyUserViaAlias(res, res, decoded.data)
-    } catch (e) {
-        res.render('login')
-    }
+  try {
+    var token = new Cookies(req, res).get('currentUser')
+    let decoded = jwt.verify(token, config.secret)
+    verifyUserViaAlias(res, res, decoded.data)
+  } catch (e) {
+    res.render('login')
+  }
 })
 
 router.get('/receive', (req, res) => {
-    var clientVariables = {}
-    clientVariables.snippetcontents = []
+  var clientVariables = {}
+  clientVariables.snippetcontents = []
+  var redirectID = 0
+  // Need to load snippet data from the database to display on the page.
+  database.getRedirect(redirectID).then(redirect => {
+    redirect = redirect[0]
+    var snippets = JSON.parse(redirect.snippetids)
+    // For each snippet, retrieve the snippet content ID.
+    snippets.forEach((entry, index) => {
+      database.getSnippet(entry).then(snippet => {
+        snippet = snippet[0]
 
-    var redirectID = 0
+        // Retrieve the snippet content.
+        database.getSnippetContent(snippet.contentid).then(snippetcontent => {
+          snippetcontent = snippetcontent[0]
 
-    // Need to load snippet data from the database to display on the page.
-    database.getRedirect(redirectID).then(redirect => {
-        redirect = redirect[0]
-        var snippets = JSON.parse(redirect.snippetids)
-        // For each snippet, retrieve the snippet content ID.
-        snippets.forEach((entry, index) => {
-            database.getSnippet(entry).then(snippet => {
-                snippet = snippet[0]
+          console.log('server: Rendering receive, snippetcontent.id: ', snippetcontent.id)
 
-                // Retrieve the snippet content.
-                database.getSnippetContent(snippet.contentid).then(snippetcontent => {
-                    snippetcontent = snippetcontent[0]
+          clientVariables.snippetcontents.push({
+            'description': snippetcontent.description,
+            'content': snippetcontent.content,
+            'id': snippetcontent.id,
+            'parentid': snippet.id
+          })
 
-                    console.log('server: Rendering receive, snippetcontent.id: ', snippetcontent.id)
-
-                    clientVariables.snippetcontents.push({
-                        'description': snippetcontent.description,
-                        'content': snippetcontent.content,
-                        'id': snippetcontent.id,
-                        'parentid': snippet.id
-                    })
-
-                    // Only return final source if it's final iteration to prevent loss.
-                    if (index === snippets.length - 1) {
-                        // Send the created page back to user after loading all the variables,
-                        // with a slight delay to prevent further problems.
-                        setTimeout(function () {
-                            res.render('receive', clientVariables)
-                        }, 100)
-                    }
-                })
-            })
+          // Only return final source if it's final iteration to prevent loss.
+          if (index === snippets.length - 1) {
+            // Send the created page back to user after loading all the variables,
+            // with a slight delay to prevent further problems.
+            setTimeout(function () {
+              res.render('receive', clientVariables)
+            }, 100)
+          }
         })
+      })
     })
+  })
 })
 
 router.get('/send', (req, res) => {
-    res.render('send')
+  res.render('send')
 })
 
 router.get('/stats', (req, res) => {
-    res.render('stats')
+  res.render('stats')
 })
 
 // Login authentication
 // Gets the username and password of input and calls authentication function
 router.post('/login', async function (req, res) {
-    var username = req.body.username
-    var password = req.body.password
-    await authenticate(res, req, username, password)
+  var username = req.body.username
+  var password = req.body.password
+  await authenticate(res, req, username, password)
 })
 
 async function verifyUserViaAlias(res, req, alias) {
-    var authentication = database.getRedirectViaAlias(alias, false)
-    authentication.then(async function (result) {
-        if (result.length > 0) {
-            if (
-                result[0].alias === alias
-            ) {
-                res.render('index')
-            } else {
-                res.render('login')
-            }
-        } else {
-            res.render('login')
-        }
-    })
+  var authentication = database.getRedirectViaAlias(alias, false)
+  authentication.then(async function (result) {
+    if (result.length > 0) {
+      if (
+        result[0].alias === alias
+      ) {
+        res.render('index')
+      } else {
+        res.render('login')
+      }
+    } else {
+      res.render('login')
+    }
+  })
 }
 
 // Authenticates username and password for login
 async function authenticate(res, req, username, password) {
-    let sqlQuery = 'SELECT * FROM Login WHERE username = ?'
-    let sqlData = username
-    var authentication = database.sqlGet(sqlQuery, sqlData, false)
-    authentication.then(async function (result) {
-        if (result.length > 0) {
-            if (
-                result[0].username === username &&
-                await database.compareHash(password + result[0].salt, result[0].password)
-            ) {
-                await generateJWT(res, req, result[0].redirectid, 'currentUser')
-                res.render('index')
-            } else {
-                res.render('login')
-            }
-        } else {
-            res.render('login')
-        }
-    })
+  // let sqlQuery = 'SELECT * FROM Login WHERE username = ?'
+  // let sqlData = username
+  var authentication = database.getUserData(username, false)
+  authentication.then(async function (result) {
+    if (result.length > 0) {
+      if (
+        result[0].username === username &&
+        await database.compareHash(password + result[0].salt, result[0].password)
+      ) {
+        await generateJWT(res, req, result[0].redirectid, 'currentUser')
+        res.render('index')
+      } else {
+        res.render('login')
+      }
+    } else {
+      res.render('login')
+    }
+  })
 }
 
 async function generateJWT(res, req, redirectid, cookieName) {
-    console.log(redirectid)
-    var alias = await database.getRedirect(redirectid).then(res => {
-        console.log(res[0])
-        return res[0].alias
-    })
-    console.log(redirectid)
-    var token = jwt.sign({
-        data: alias
-    }, config.secret, {
-        expiresIn: 30
-    }, {
-        algorithm: 'RS256'
-    })
-    res.cookie(cookieName, token)
+  var alias = await database.getRedirect(redirectid).then(res => {
+    return res[0].alias
+  })
+  var token = jwt.sign({
+    data: alias
+  }, config.secret, {
+    expiresIn: 30
+  }, {
+    algorithm: 'RS256'
+  })
+  res.cookie(cookieName, token)
 }
 
 router.get('/send', function (req, res) {
-    res.render('send')
+  res.render('send')
 })
 
 router.get('/stats', function (req, res) {
-    res.render('stats')
+  res.render('stats')
 })
 
 app.use('/', router)
--- site/tests/server.tests.js
+++ site/tests/server.tests.js
@@ -10,29 +10,22 @@ chai.use(chaiHttp)
 // server.connectToServer()
 
 describe('Cookie Creation and Validation', async function () {
-  it('Cookies are created and validated properly.', async function () {
-    // The redirect needs to be created first as the login points to it.
-    var redirectid = await database.createRedirect('TestAlias2', 1, true).then(result => {
-      return result
-    })
-    // await server.generateJWT(res, req, redirectid, 'testUser', true)
-    // console.log(res)
-    chai.request('localhost:7000').post('/login').send({
-      username: 'TestUsername',
-      password: 'TestPassword'
-    }).end(function (req, res) {
-      console.log(res)
-      // if (err) {
-      //   throw err
-      // }
-      var token = new Cookies(req, res).get('currentUser')
-      let decoded = jwt.verify(token, config.secret)
-      expect(decoded.alias).to.equal('TestAlias2')
-      done()
-    })
-    var redirectRemoved = await database.removeRedirect(redirectid, true).then(res => {
-      return res
-    })
-    expect(redirectRemoved).to.not.equal(null)
+  it('Cookies are created and validated properly.', async function (done) {
+    chai.request('localhost:7000')
+      .post('/')
+      .send({
+        username: 'TestUsername2',
+        password: 'TestPassword2'
+      })
+      .end(async function (req, res) {
+        await server.generateJWT(res, req, 1, 'testUser', true)
+
+        var token = new Cookies(req, res).get('testUser')
+        console.log(token)
+        let decoded = jwt.verify(token, config.secret)
+        console.log(decoded)
+        expect(decoded.alias).to.equal('TestAlias2')
+      })
+    done()
   })
 })
