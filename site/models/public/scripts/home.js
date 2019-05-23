5e3bcc849651fe3be372a92dc7e2e6a60518f293
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-05-23 11:07:32 +0100

    Fixed scaling images with funky image size rations in inbox center

--- site/models/public/scripts/home.js
+++ site/models/public/scripts/home.js
@@ -4,14 +4,14 @@ var $ = function (id) { return document.getElementById(id) }
 console.log('Index script started.')
 
 var infoTextList = ['Welcome to Simun!',
-  'Simun is a depersonalized image sharing network',
-  '"A picture is worth a thousand words"',
-  'Images can be sent from the Outbox. When sent, they turn from images to "snippets"',
-  'For every image there are multiple snippets with different fates',
-  'Creating or forwarding a snippet sends it to two users at random',
-  'Deleting a snippet removes it from the system; the image may exist in other snippets though',
-  'You can decide the fate of snippets sent to you from your inbox',
-  'The most forwarded images are displayed in the global tab for everyone to see',
+  'Simun is a depersonalized image sharing network.',
+  '"A picture is worth a thousand words".',
+  'Images can be sent from the Outbox. When sent, they turn from images to "snippets".',
+  'For every image there are multiple snippets with different fates.',
+  'Creating or forwarding a snippet sends it to two users at random.',
+  'Deleting a snippet removes it from the system; the image may exist in other snippets though.',
+  'You can decide the fate of snippets sent to you from your inbox.',
+  'The most forwarded images are displayed in the global tab for everyone to see.',
   'Simun says happy snippeting!']
 
 var infoTextCounter = 0
