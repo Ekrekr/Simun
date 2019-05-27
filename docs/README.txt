4cd4388eb05f651b5b1014c39597951b7825b265
Elias Kassell Raymond <eliaszoharkr@gmail.com>
2019-05-27 12:10:57 +0100

    Updated zip it script, final changes to report

--- docs/README.txt
+++
@@ -1,5 +0,0 @@
-Hi Ian,
-
-I’ve provided a full report as a separate pdf, it’s a bit long though so I’ve put together the specific categories and grades in the report-supplementary document.
-
-Elias
--- zip-it.sh
+++ zip-it.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Zips the site folder for submission, ignoring files that should be ignored.
-zip -r simun.zip site README.md LICENSE docs -x *.git* *node_modules* *.DS_Store*
+zip -r simun.zip site README.md docs -x *.git* *node_modules* *.DS_Store*
